9dd9dd213726f847bce4667ba0ad0ced50c59e54
Chirag Boghara <63946430+ChiragBoghara@users.noreply.github.com>
2023-03-06 12:10:28 +0530

    Update search_in_sorted_rotated_array.cpp

--- search_in_sorted_rotated_array.cpp
+++ search_in_sorted_rotated_array.cpp
@@ -5,21 +5,21 @@ public:
 	    int start = 0;
 	    int end = n-1;
 	    while(start<=end){
-    	    if(nums[start]<=nums[end]){
-    	        return start;
-    	    }
-	        int mid = start + (end-start)/2;
-	        int prev = (mid+n-1)%n;
-	        int next = (mid+1)%n;
-	        
-	        if(nums[mid]<=nums[prev] && nums[mid]<=nums[next]){
-                return mid;
-            }
-            if(nums[start]<=nums[mid]){
-                start=mid+1;
-            }else if(nums[mid]<=nums[end]){
-                end=mid-1;
-            }
+		if(nums[start]<=nums[end]){
+			return start;
+		}
+		int mid = start + (end-start)/2;
+		int prev = (mid+n-1)%n;
+		int next = (mid+1)%n;
+
+		if(nums[mid]<=nums[prev] && nums[mid]<=nums[next]){
+			return mid;
+		}
+		if(nums[start]<=nums[mid]){
+			start=mid+1;
+		}else if(nums[mid]<=nums[end]){
+			end=mid-1;
+		}
 	    }
 	    return 0; 
 	}
